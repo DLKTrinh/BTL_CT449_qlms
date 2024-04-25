412dfcd49a57870f795ab873878de6b7d6712c01
DLKTrinh <125120227+DLKTrinh@users.noreply.github.com>
2024-04-25 21:26:19 +0700

    nhanvien

---
+++ src/components/NhanVienCard.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    props: {
+        nhanvien: { type: Object, required: true },
+    },
+};
+</script>
+
+<template>
+    <div>
+        <div class="p-1">
+            <strong>Tên:</strong>
+            {{ nhanvien.name }}
+        </div>
+        <div class="p-1">
+            <strong>Mật khẩu:</strong>
+            {{ nhanvien.password }}
+        </div>
+        <div class="p-1">
+            <strong>Vị trí:</strong>
+            {{ nhanvien.position }}
+        </div>
+        <div class="p-1">
+            <strong>Địa chỉ:</strong>
+            {{ nhanvien.address }}
+        </div>
+        <div class="p-1">
+            <strong>Điện thoại:</strong>
+            {{ nhanvien.phone }}
+        </div>
+    </div>
+</template>
